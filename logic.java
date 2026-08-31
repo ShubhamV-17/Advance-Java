650b9583cf573c9a37273ad15582f269d567d3d3
Shubham Vishwakarma <shubham10137@gmail.com>
2026-08-31 21:00:28 +0530

    Initial commit

--- logic.java
+++
@@ -1,7 +0,0 @@
-public class logic {
-    public static void main(String[] args) {
-        System.out.println(3 > 2 && 5 > 0);
-        System.out.println(3 > 2);
-        System.out.println(!(3 > 2));
-    }
-}
