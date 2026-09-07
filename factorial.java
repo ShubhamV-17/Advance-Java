3a39fa1280f1296c520d0942fca64abecc2bdebd
Shubham Vishwakarma <shubham10137@gmail.com>
2026-09-07 22:12:53 +0530

    today factorial done!

--- factorial.java
+++ factorial.java
@@ -1,3 +1,17 @@
 public class factorial {
-    
+
+    public static int printingfactorial(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        int z = printingfactorial(10);
+        System.out.println(z);
+
+    }
+
 }
