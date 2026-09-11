a308ab62b5e9789ea25d24fc207c7752be12ba37
shubham vishwakarma <shubham10137@gmail.com>
2026-09-11 23:35:09 +0530

    Add Java programs

--- FUNCTION OVERLOADING/inrange.java
+++ FUNCTION OVERLOADING/inrange.java
@@ -1,3 +1,32 @@
 public class inrange {
 
+    public static boolean isprime(int n) {
+
+        if (n < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void primeinrange(int n) {
+
+        for (int i = 2; i <= n; i++) {
+            if (isprime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isprime(16));
+
+    }
 }
