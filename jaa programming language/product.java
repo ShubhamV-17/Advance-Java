b7bfc7669b4eb98098529e53f504b352d53f2467
shubham vishwakarma <shubham10137@gmail.com>
2026-09-12 10:43:36 +0530

    Add Java programs

--- jaa programming language/product.java
+++ jaa programming language/product.java
@@ -1,12 +1,16 @@
-import java.util.*;
-
 public class product {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = a * b;
-        System.out.println("a*b=" + c);
+
+    public static int productoftwonumber(int a, int b) {
+        int product = a * b;
+        return product;
+
     }
 
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int prod = productoftwonumber(a, b);
+        System.out.println("a * b = " + prod);
+
+    }
 }
