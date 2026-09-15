4e28d83e6cf528956970380cc6f03be16520497d
shubham vishwakarma <shubham10137@gmail.com>
2026-09-15 23:20:20 +0530

    Add Advance Pattern programs

---
+++ ADVANCE PATTERN/halfprymid.java
@@ -0,0 +1,14 @@
+public class halfprymid {
+    public static void half_pryamid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        half_pryamid(5);
+    }
+}
