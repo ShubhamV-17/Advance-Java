2a0f89166ae38c58a176803c1015303450a83d10
shubham vishwakarma <shubham10137@gmail.com>
2026-09-18 21:29:16 +0530

    Add Java programs

--- ARRAY/creation.java
+++ ARRAY/creation.java
@@ -2,5 +2,10 @@ public class creation {
     public static void main(String[] args) {
         int marks[] = new int[50];
         int numbers[] = { 1, 2, 3 };
+
+        System.out.println("Numbers in the array:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
---
+++ ARRAY/linearsearch.java
@@ -0,0 +1,23 @@
+public class linearsearch {
+    public static int linear_search(int number[], int key) {
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int marks[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int key = 10;
+
+        int index = linear_search(marks, key);
+        if (index == -1) {
+            System.out.println(" NOT FOUND");
+        } else {
+            System.out.println(" Key is at index : " + index);
+        }
+    }
+}
