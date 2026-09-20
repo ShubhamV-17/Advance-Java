6b7b68ccfffe313b884f6f674e0a98a9250469a5
shubham vishwakarma <shubham10137@gmail.com>
2026-09-20 11:38:36 +0530

    Update changes

--- ARRAY/pair.java
+++ ARRAY/pair.java
@@ -1,5 +1,16 @@
 public class pair {
+    public static void print_pairs(int numbers[]){
+        for(int i= 0; i<numbers.length; i++){
+            int current= numbers[i];
+            for(int j=i+1; j<numbers.length;j++){
+                System.out.print("("+ current + ","+ numbers[j]+")");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
+        int number[] = {2,4,6,8,10};
+        print_pairs(number);
 
     }
 }
