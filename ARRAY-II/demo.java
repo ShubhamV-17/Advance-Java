8c05add6899a15fc23c62472f955551f2caea79f
shubham vishwakarma <shubham10137@gmail.com>
2026-09-24 21:51:08 +0530

    Update files

--- ARRAY-II/demo.java
+++ ARRAY-II/demo.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class demo {
     public static void main(String[] args) {
-        System.out.println("hello duniya");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            int x = sc.nextInt();
+            int f = 1;
+            for (int j = 1; j <= x; j++) {
+                f = f * j;
+            }
+            System.out.println(f);
+        }
     }
 
 }
